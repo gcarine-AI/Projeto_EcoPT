3087d6cad016887216183922c33140b8e2d146d8
Carine Gonçalves <g.carine@gmail.com>
2026-03-27 15:38:49 +0000

    refactor(config): update to provideAnimationsAsync and fix unused providers

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth-interceptor';
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideAnimationsAsync,cd 
     provideHttpClient(withInterceptors([authInterceptor])),
   ],
 };
